28ed9e96018ea8ff66231d690b85390eaf124d39
Cindy <19cindyl@gmail.com>
2023-05-06 22:02:08 -0400

    redid form

--- cocktailsApp/src/app/app.component.ts
+++ cocktailsApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormGroup,  FormBuilder, FormControl, Validators, RequiredValidator } from '@angular/forms'; 
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,18 +8,40 @@ import { FormGroup,  FormBuilder, FormControl, Validators, RequiredValidator } f
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// export class AppComponent {
+//   title = 'cocktailsApp';
+//   //Create FormGroup
+//   requiredForm!: FormGroup;
+//   constructor(private fb: FormBuilder) {
+//      this.myForm();
+//   }
+
+//   //Create required field validator for name
+//   myForm() {
+//      this.requiredForm = this.fb.group({
+//      name: ['', Validators.required ]
+//      });
+//   }
+// }
+
 export class AppComponent {
-  title = 'cocktailsApp';
-  //Create FormGroup
-  requiredForm!: FormGroup;
-  constructor(private fb: FormBuilder) {
-     this.myForm();
+  form: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
   }
 
-  //Create required field validator for name
-  myForm() {
-     this.requiredForm = this.fb.group({
-     name: ['', Validators.required ]
-     });
-  }
+  // submitForm() {
+  //   var formData: any = new FormData();
+  //   formData.append('name', this.form.get('name').value);
+  //   formData.append('avatar', this.form.get('avatar').value);
+  //   this.http
+  //     .post('http://localhost:4000/api/create-user', formData)
+  //     .subscribe({
+  //       next: (response) => console.log(response),
+  //       error: (error) => console.log(error),
+  //     });
+  // }
 }
